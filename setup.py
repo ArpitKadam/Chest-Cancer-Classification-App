8936c0d8f9900798b4c13a31638d5b9213c6678f
Arpit Kadam <arpitkadam922@gmail.com>
2025-06-25 01:03:47 +0530

    Requirements and Setup File Commit

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+from typing import List
+
+def get_requirements() -> List[str]:
+    requirements = []
+    with open('requirements.txt') as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.replace("\n","") for req in requirements]
+        
+        if '-e .' in requirements:
+            requirements.remove('-e .')
+
+    return requirements
+
+setup(
+    name = "hest-Cancer-Classification-App",
+    version = "0.0.0",
+    author = "ArpitKadam",
+    author_email="arpitkadam922@gmail.com",
+    description="Package for Chest-Cancer-Classification-App",
+    url="https://github.com/ArpitKadam/Chest-Cancer-Classification-App",
+    packages=find_packages(),
+    install_requires = get_requirements(),
+)
